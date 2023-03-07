9c40c6b67a649607636345c22cd0ad5ec84df862
Jerry Toscano <Jerry.Toscano@strongmind.com>
2023-03-07 03:26:36 -0700

    Function declartions parsed

--- NLox.Interpreter/Parser.cs
+++ NLox.Interpreter/Parser.cs
@@ -26,13 +26,34 @@ public class Parser
     {
         try
         {
-            return await (this.Match(TokenType.Var) ? VariableDeclaration() : Statement());
+            return await (this.Match(TokenType.Var) 
+                              ? VariableDeclaration() 
+                              : this.Match(TokenType.Fun) 
+                                ? Function("function") 
+                                : Statement());
         }
         catch (ParsingException)
         {
             this.Synchronize();
             return null;
         }
+    }
+
+    private async Task<IStatement> Function(string kind)
+    {
+        var name = await this.Consume(TokenType.Identifier, $"Expect {kind} name."); 
+        await this.Consume(TokenType.LeftParen, $"Expect '(' after {kind} name.");
+        List<Token> parameters = new();
+        if (!this.Check(TokenType.RightParen))
+            do
+            {
+                if (parameters.Count >= 255)
+                    await this.Error(this.Peek, "Can't have more than 255 parameters.");
+                parameters.Add(await this.Consume(TokenType.Identifier, "Expect parameter name."));
+            } while (this.Match(TokenType.Comma));
+        await this.Consume(TokenType.RightParen, "Expect ')' after parameters.");
+        await this.Consume(TokenType.LeftBrace, $"Expect '{{' before {kind} body.");
+        return new Function(name, parameters, (Block)await this.Block());
     }
 
     private async Task<IStatement> If()
---
+++ NLox.Interpreter/Statements/Function.cs
@@ -0,0 +1,3 @@
+﻿namespace NLox.Interpreter.Statements;
+
+public record Function(Token Name, IList<Token> Parameters, Block Body) : IStatement;
